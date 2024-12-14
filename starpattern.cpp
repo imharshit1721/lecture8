54b50c1283aae542fd4eec278333c5791288ee8d
imharshit1721 <118714362+imharshit1721@users.noreply.github.com>
2024-12-14 20:20:48 +0530

    Create starpattern.cpp
    star patterns  in c++

---
+++ starpattern.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    
+    int row , col , n;
+    
+    row = 1;
+    while(row <= 5){
+        int space  = 5 - row;
+        while(space){
+            cout<<" ";
+            space = space - 1; 
+        }
+        
+        int col = 1;
+        while(col <= row){
+            cout<<"* ";
+            col = col  + 1;
+        }
+        
+        cout<<endl;
+        row = row + 1;
+    }
+    
+   row = 5;
+
+    while(row >= 1){
+        int space = 5 - row;
+        while(space > 0){
+            cout<<" ";
+            space = space - 1;
+        }
+        
+        
+         col = 1;
+         while(col <= row){
+             cout<<"* ";
+             col = col  + 1;
+         }
+        
+        cout<<endl;
+        row = row  - 1; 
+    }
+}
+
+
+
+
+------------------------------------------
+output
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
